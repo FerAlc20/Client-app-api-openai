b043b99972138fffd9c8c1225813ce8889512bbe
Fernanda Alcantara <155588642+FerAlc20@users.noreply.github.com>
2025-06-01 11:23:27 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Envía el mensaje a la API de OpenAI
             try {
-                const response = await fetch('http://44.222.213.244/api-gpt-php/endpoints/chat.php', { // Ruta absoluta corregida
+                const response = await fetch('http://3.92.87.213/api-gpt-php/endpoints/chat.php', { // Ruta absoluta corregida
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
